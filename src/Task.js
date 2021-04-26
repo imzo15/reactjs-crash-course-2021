10e65264a8e1d54f63974fd8cf48d3764826493d
Irving Zamora <imzo15@hotmail.com>
2021-04-25 21:00:36 -0600

    Added react-icons

--- src/Task.js
+++ src/Task.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import {FaTimes} from 'react-icons/fa'
 
 const Task = (props) => {
     return(
         <div className='task'>
-            <h3>{props.task.title}</h3>
+            <h3>{props.task.title} <FaTimes style={closeButtonStyle} /></h3>
             <p>{props.task.day}</p>
         </div>
     )
+}
+
+const closeButtonStyle = {
+    color: 'red',
+    cursor: 'pointer'
 }
 
 export default Task
